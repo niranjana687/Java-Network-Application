29cadf79217baf8e5571a81c90678f6f992a38f2
niranjana687 <niranjana687@gmail.com>
2021-06-05 21:29:22 +0530

    Server set up

--- src/simpleTCP/Server.java
+++ src/simpleTCP/Server.java
@@ -1,16 +1,32 @@
 package simpleTCP;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
 
 	public Server() throws Exception {
-		ServerSocket serverSocket = new ServerSocket(6969); //opening a new port
+		ServerSocket serverSocket = new ServerSocket(6969); //opening a new port.
 		System.out.println("Port 6969 is open");
 		
 		Socket socket = serverSocket.accept();
 		System.out.println("Client: " + socket.getInetAddress() + " has connected");
+		
+		// I/O Buffers.
+		BufferedReader inSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		PrintWriter outSocket = new PrintWriter(new OutputStreamWriter (socket.getOutputStream()));
+		
+		outSocket.println("Welcome"); //send "welcome" to the client.
+		String message = inSocket.readLine();
+		System.out.println("Client says: " + message);
+		
+		socket.close();
+		System.out.println("Socket is closed");
+				
 	}
 	
 	public static void main(String[] args) {
